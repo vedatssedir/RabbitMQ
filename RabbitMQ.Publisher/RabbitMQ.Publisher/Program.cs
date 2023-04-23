9d5661b37eba672feb18da46a4246cbb70de8cf8
vedatssedir <vedatssedir@gmail.com>
2023-04-24 00:47:17 +0300

    created RabbitMQ folder in Publisher and Consumer Project

---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+//Baglantı olusturma
+factory.Uri = new("amqps://lvllnjmp:48Gu0NlBOizb1W7Uaxf0PUVVTsoMsAWe@toad.rmq.cloudamqp.com/lvllnjmp");
+//Baglantı da kanal acma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue olusturma
+
+channel.QueueDeclare(queue: "example-queue 1");
